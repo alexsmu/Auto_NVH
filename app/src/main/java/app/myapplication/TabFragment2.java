f17de23fba89e1229081cf2755f9ad7b01644660
mavs0890 <marlon.alejandro.vilorio@gmail.com>
2016-05-21 10:52:23 -0600

    Started changes on Tab#2

--- app/src/main/java/app/myapplication/TabFragment2.java
+++ app/src/main/java/app/myapplication/TabFragment2.java
@@ -5,15 +5,51 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class TabFragment2 extends Fragment {
     View mMain = null;
+    private ToggleButton toggleRealTime, togglePlayBack;
+    private TextView textTitle;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         mMain = inflater.inflate(R.layout.tab_fragment_2, container, false);
+        addListenerToToggleButtons();
         return mMain;
+    }
+
+    void addListenerToToggleButtons() {
+        toggleRealTime = (ToggleButton) mMain.findViewById(R.id.toggleRealTime);
+        togglePlayBack = (ToggleButton) mMain.findViewById(R.id.togglePlayBack);
+        textTitle = (TextView) mMain.findViewById(R.id.textTitle);
+
+        toggleRealTime.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                toggleRealTime.setChecked(true);
+                togglePlayBack.setChecked(false);
+                Toast.makeText(getActivity().getApplicationContext(), "Real Time Session",
+                        Toast.LENGTH_SHORT).show();
+                textTitle.setText("Real Time Session");
+            }
+        });
+
+        togglePlayBack.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                togglePlayBack.setChecked(true);
+                toggleRealTime.setChecked(false);
+                Toast.makeText(getActivity().getApplicationContext(), "Playback Session",
+                        Toast.LENGTH_SHORT).show();
+                textTitle.setText("Playback Session");
+            }
+        });
     }
 
     @Override
